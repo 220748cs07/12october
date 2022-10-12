640234fb9a36e4f95265afd889acffb9928ef9dd
Tsay Artem <113498930+220748cs07@users.noreply.github.com>
2022-10-12 11:27:46 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,153 @@
+//
+//  main.cpp
+//  12october
+//
+//  Created by Say Artyom on 12/10/22.
+//
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main(){
+    //Problem 1
+//    for(int i = 0; i<10 ; i++){
+//        cout<<i+1<<" ";
+//    }
+    
+    //Problem 2
+//    int n = 0;
+//    for(int i = 0; i<10 ; i++){
+//        n+=i;
+//        cout<<i+1<<" ";
+//
+//    }
+//    cout<<"\n"<<"Sum - " << n << endl;
+    
+    //Problem 3
+//    for(int i = 1; i<=100; i++ ){
+//        if(i%5 != 0){
+//            cout<<i<<" ";
+//        }
+//    }
+    
+    //Problem 4
+     
+//    int n = 0;
+//    cout<<"Number - ";
+//    cin>>n;
+//    if(n >1 ){
+//        for(int i=2; i<=n; i++){
+//            if(n % n == 0 && n%i != 0){
+//                cout<<"Prime"<<endl;
+//                break;
+//            }
+//            else if((n % n == 0) && (n%i == 0)){
+//                cout<<"Not prime"<<endl;
+//                break;
+//            }
+//        }
+//    }
+//    else{
+//        cout<<"n=1"<< endl;
+//    }
+
+    
+    //Problem 5
+    
+//    int n = 0;
+//    cout<<"Number - ";
+//    cin>>n;
+//    for(int i = 1; i<=n; i++){
+//        cout<<i<<"*"<<i<<"="<<i*i<<endl;
+//    }
+    //Problem 6
+    
+//    int n,max = 0;
+//    cout<< "Enter a list of inter. terminated by 0 - ";
+//    cin>>n;
+//    while(n != 0){
+//        if(max<n)
+//            max = n;
+//        cin>>n;
+//    }
+//    cout<<"Max - "<<max<<endl;
+//    return 0;
+    
+    //Problem 7
+//    char sym ;
+//        float a,b=0;
+//
+//        cout<<"Put ur operations + or - or * or / : ";
+//        cin>>sym;
+//        cout<<"First number - ";
+//        cin>>a;
+//        cout<<"Second number - ";
+//        cin>>b;
+//        switch (sym) {
+//            case '+':
+//                cout<< a+b << endl;
+//                break;
+//            case '-':
+//                cout<< a-b<< endl;
+//                break;
+//            case '*':
+//                cout<< a*b<< endl;
+//                break;
+//            case '/':
+//                cout<< a/b<< endl;
+//                break;
+//
+//
+//
+//
+//
+//            default:
+//                cout<<"Not numbers/Error syntax";
+//                break;
+
+    
+    //Problem 8
+    
+//    int n = 0;
+//    cout<<"n - ";
+//    cin>>n;
+//
+//       for(int i=0; i<n; i++){
+//           string z = "#";
+//           z = "";
+//           for(int a=0; a<n;a++){
+//               z += "#";
+//           }
+//           cout<< z<< endl;
+//
+//       }
+       
+    
+    
+    //Problem 9
+    
+//    int n = 0;
+//    cout<<"n - ";
+//    cin>>n;
+//    string z = "#";
+//    for(int i=0; i<n; i++){
+//           cout<<z<<endl;
+//           z += "#";
+//       }
+  //Problem 13
+    
+    //Problem 14
+//    int n = 0;
+//    cout<<"n - ";
+//    cin>>n;
+//
+//    for(int i=1; i<=n; i++){
+//        string z = "";
+//        for(int a=0; a<i;a++){
+//            z += to_string(i);
+//            }
+//        cout<<z<<endl;
+//       }
+    
+}
+
